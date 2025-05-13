d082e48df1eebc6e9032a7bc8a673e3dcafcc0e0
lsimek <tinjano@proton.me>
2025-05-14 01:58:12 +0200

    14-05

--- shell.nix
+++ shell.nix
@@ -1,16 +1,59 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 let
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive)
-      scheme-full
-      baskervaldx
-      newtx;
-      #(setq org-latex-compiler "lualatex")
-      #(setq org-preview-latex-default-process 'dvisvgm)
-  });
-in 
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs =  [ tex ];
-}
+  tex = (
+    pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-medium
 
+        textpos
+        etextools
+        environ
+        fmtcount
+        koma-script
+        # inputenc
+        babel
+        babel-croatian
+        datetime
+        geometry
+        amsfonts
+        # amsmath
+        # amssymb
+        # amsthm
+        csquotes
+        tcolorbox
+        pgf
+        pgfplots
+        arydshln
+        float
+        xcolor
+        # fontenc
+        breqn
+        thmtools
+        multirow
+        hyperref
+        booktabs
+        listings
+        letltxmacro
+        adjustbox
+        enumitem
+        biblatex
+        placeins
+        mathtools
+        autonum
+        url
+	subfiles
+        ; # dvisvgm
+    }
+  );
+in
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = [
+    tex
+    pkgs.bibtex-tidy
+    pkgs.texlab
+    pkgs.texmaker
+  ];
+}
